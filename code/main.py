0e5862a4a253900d161b6895edc64c9caf0203fe
LeeDoyeon98 <dyl1229@naver.com>
2021-11-25 00:37:14 +0900

    refactor function names

--- code/main.py
+++ code/main.py
@@ -13,17 +13,15 @@ from plotting import get_distance
 from clustering import clustering
 
 
-# 전체 프레임에서 bbox만큼 잘라 관절 추출(각도, 기울기는 상대적인 값이기 때문)
-# 팔이 범위를 벗어날 수 있기 때문에 가로 2배
-def get_skeleton(line, image, frame_id):
-    line = list(map(float, line))
-    line = list(map(round, line))
+def save_cropped_image_bbox(csv_line, image, frame_id):
+    csv_line = list(map(float, csv_line))
+    csv_line = list(map(round, csv_line))
 
     # 이미지 잘라서 저장
-    x = line[0] - line[2] // 2
-    y = line[1]
-    w = line[2] * 2
-    h = line[3]
+    x = csv_line[0] - csv_line[2] // 2
+    y = csv_line[1]
+    w = csv_line[2] * 2
+    h = csv_line[3]
     if x + w > image.shape[1]:
         x1 = image.shape[1]
     else:
@@ -40,6 +38,10 @@ def get_skeleton(line, image, frame_id):
     cropped_image = image[y: y1, x: x1]
     cv2.imwrite(f"../output/video/{get_video_name()}/cropped_image/{frame_id}.png", cropped_image)
 
+
+# 전체 프레임에서 bbox만큼 잘라 관절 추출(각도, 기울기는 상대적인 값이기 때문)
+# 팔이 범위를 벗어날 수 있기 때문에 가로 2배
+def get_skeleton(frame_id):
     # 잘린 이미지에 대해서 skeleton 뽑아냄
     json_file = detect_skeleton(get_video_name(),
                                 ["--image_path", f"../output/video/{get_video_name()}/cropped_image/{frame_id}.png"],
@@ -147,41 +149,40 @@ def find_adult(csv_file, frame_num):
     return adult_id, frame_num, first_frame_json, skeleton_id
 
 
-def tracking_by_skeleton(json_obj, frame_num, skeleton_id):
-    skeleton_list = get_skeleton_list()
+def track_by_skeleton(json_obj, frame_num, skeleton_id):
     # skeleton_list가 비어있으면(첫 번째 프레임이어서 성인 탐지가 안되어 있으면)
     # 무조건 성인으로 탐지한 skeleton 값을 집어넣음
+    skeleton_list = get_skeleton_list()
     if len(skeleton_list) == 0:
         set_prev_adult_point(get_current_adult_point())
-
         json_obj[0]['person'][skeleton_id]['person_id'] = 0
         append_skeleton_list({"frame_id": frame_num, "person": [json_obj[0]['person'][skeleton_id]]})
-        crop([skeleton_list[-1]])
+        save_cropped_image_skeleton([skeleton_list[-1]])
         return
 
     distance, key_count = get_distance(json_obj, skeleton_id)
-
     w, h = get_frame_size()
     skipped_frame_num = frame_num - skeleton_list[-1]['frame_id']   # 현재 프레임과의 차이
     max_range = ((get_prev_adult_head_len()**2 * 56) / (165 * h)) * skipped_frame_num
+
     if key_count > 0 and distance / key_count < max_range:
         set_prev_adult_point(get_current_adult_point())
-
         json_obj[0]['person'][skeleton_id]['person_id'] = 0
         append_skeleton_list({"frame_id": frame_num, "person": [json_obj[0]['person'][skeleton_id]]})
-        crop([skeleton_list[-1]])
+        save_cropped_image_skeleton([skeleton_list[-1]])
     return
 
 
-def crop(skeleton_list):
+def save_cropped_image_skeleton(skeleton_list):
     i = 0
     while i < len(skeleton_list):
         frame_num = skeleton_list[i]['frame_id']
         image = cv2.imread(f"../output/video/{get_video_name()}/frames/{frame_num}.png")
-        x1 = int(skeleton_list[i]['person'][0]['keypoint']['LShoulder']['x'])
-        x2 = int(skeleton_list[i]['person'][0]['keypoint']['RShoulder']['x'])
-        y1 = int(skeleton_list[i]['person'][0]['keypoint']['Head']['y'])
-        y2 = int(skeleton_list[i]['person'][0]['keypoint']['Chest']['y'])
+        tmp_keypoint = skeleton_list[i]['person'][0]['keypoint']
+        x1 = int(tmp_keypoint['LShoulder']['x'])
+        x2 = int(tmp_keypoint['RShoulder']['x'])
+        y1 = int(tmp_keypoint['Head']['y'])
+        y2 = int(tmp_keypoint['Chest']['y'])
 
         if x1 > x2:
             x1, x2 = swap(x1, x2)
@@ -189,7 +190,8 @@ def crop(skeleton_list):
             y1, y2 = swap(y1, y1)
 
         cropped_image = image[y1: y2, x1: x2]
-        cv2.imwrite(f"../output/video/{get_video_name()}/cropped_image/{frame_num}.png", cropped_image)
+        CROPPED_IMAGE_PATH = f"../output/video/{get_video_name()}/cropped_image/{frame_num}.png"
+        cv2.imwrite(CROPPED_IMAGE_PATH, cropped_image)
         i += 1
 
 
@@ -225,11 +227,12 @@ def handle_first_frame_info(BBOX_CSV_PATH):
         # frame_num이 total_frame을 넘었음(영상이 끝남)
         if adult_id == -1:
             break
+
         # skeleton 추출은 되지만 bbox가 없는 경우 skeleton 자체를 skeleton_list에 append 해줌
         if adult_id == -2:
             with open(adult_json, 'r') as f:
                 json_obj = json.load(f)
-            tracking_by_skeleton(json_obj, frame_num, skeleton_id)
+            track_by_skeleton(json_obj, frame_num, skeleton_id)
             frame_num += 1
     return adult_id, frame_num
 
@@ -255,17 +258,18 @@ def get_first_adult_id(BBOX_CSV_PATH):
         while csv_idx < csv_size and int(csv_list[csv_idx][0]) < frame_num + 1:
             csv_idx += 1
 
-        tmp_list = []
+        csv_tmp_list = []
         # frame_id가 동일한 line들을 list로 만듦
         if is_frame_passed:
             # 다음 프레임으로 넘어가기 전까지 반복
             while int(csv_list[csv_idx][0]) == frame_num + 1:
                 if csv_idx >= csv_size: break
-                tmp_list.append(csv_list[csv_idx])
+                csv_tmp_list.append(csv_list[csv_idx])
                 csv_idx += 1
 
         # 검출된 bbox가 없는 경우
-        if len(tmp_list) == 0:
+        if len(csv_tmp_list) == 0:
+            # 306번째 줄과 동일한데 함수로 어떻게 뺄 수 있을까?
             before_frame_num = frame_num
             adult_id, frame_num, adult_json, skeleton_id = find_adult(BBOX_CSV_PATH, frame_num)
             # find_adult 함수 내부에서 frame_num이 넘어갔는지 확인
@@ -279,7 +283,7 @@ def get_first_adult_id(BBOX_CSV_PATH):
             if adult_id == -2:
                 with open(adult_json, 'r') as f:
                     json_obj = json.load(f)
-                tracking_by_skeleton(json_obj, frame_num, skeleton_id)
+                track_by_skeleton(json_obj, frame_num, skeleton_id)
                 frame_num += 1
                 is_frame_passed = True
             elif adult_id >= 0:
@@ -287,12 +291,14 @@ def get_first_adult_id(BBOX_CSV_PATH):
             continue
 
         image = cv2.imread(f"../output/video/{get_video_name()}/frames/{frame_num}.png")
-        not_detected = True
+        not_detected = True  # saved_adult_id 의 bbox가 감지되지 않았으면 True
 
-        for item in tmp_list:
+        for csv_tmp_line in csv_tmp_list:
             # 추적 대상 tracking하며 관절 추출
-            if int(item[1]) == saved_adult_id:
-                extend_skeleton_list(get_skeleton(item[2:6], image, frame_num))
+            if int(csv_tmp_line[1]) == saved_adult_id:
+                # cropped image 저장하고 skeleton extend하기
+                save_cropped_image_bbox(csv_tmp_line[2:6], image, frame_num)
+                extend_skeleton_list(get_skeleton(frame_num))
                 not_detected = False
                 # break 넣어도 되지 않나?
 
@@ -311,7 +317,7 @@ def get_first_adult_id(BBOX_CSV_PATH):
             if adult_id == -2:
                 with open(adult_json, 'r') as f:
                     json_obj = json.load(f)
-                tracking_by_skeleton(json_obj, frame_num, skeleton_id)
+                track_by_skeleton(json_obj, frame_num, skeleton_id)
                 frame_num += 1
                 is_frame_passed = True
             elif adult_id >= 0:
@@ -328,6 +334,7 @@ def make_violence_video(SKELETON_JSON_PATH):
     time_list = clustering(SKELETON_JSON_PATH)
     pathOut = f'../output/final_results/{get_video_name()}.avi'
     out = cv2.VideoWriter(pathOut, cv2.VideoWriter_fourcc(*'DIVX'), 30, get_frame_size())
+
     for lst in time_list:
         for i in range(lst[0], lst[1] + 1):
             pathIn = f'../output/video/{get_video_name()}/frames/{i}.png'
@@ -337,15 +344,16 @@ def make_violence_video(SKELETON_JSON_PATH):
 
 
 if __name__ == "__main__":
+    # argument 에 따른 데이터 초기화
     original_video_name = sys.argv[1]
+    set_video_name(original_video_name)
+    init_point()
+    init_rate()
+
     if sys.argv[2] == "n":
         original_video_path = f'../media/non-violence/{original_video_name}.mp4'
     else:
         original_video_path = f'../media/violence/{original_video_name}.mp4'
-
-    set_video_name(original_video_name)
-    init_rate()
-    init_point()
 
     # frame_size 정해줌
     cap = cv2.VideoCapture(original_video_path)
@@ -354,12 +362,14 @@ if __name__ == "__main__":
     set_frame_size(int(width), int(height))
     cap.release()
 
+    # MOT 으로 tracking 실행
     exec_MOT(original_video_path)
 
+    # 성인 id 탐색
     BBOX_CSV_PATH = f'../output/video/{get_video_name()}/final/results{get_video_name()}_0.csv'
     adult_id = get_first_adult_id(BBOX_CSV_PATH)
 
-    # json파일로 저장
+    # 성인의 관절 데이터를 json 파일로 저장
     SKELETON_JSON_PATH = f'../output/video/{get_video_name()}/results{get_video_name()}.json'
     with open(SKELETON_JSON_PATH, 'w', encoding="utf-8") as make_file:
         json.dump(get_skeleton_list(), make_file, ensure_ascii=False, indent="\t")
